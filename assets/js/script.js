a67c7d44651ce6db5f26acae2eb2fbd8d6975f9e
glenluersman <glen.luersman@gmail.com>
2021-12-13 20:52:05 -0500

    search cards

---
+++ assets/js/script.js
@@ -0,0 +1,119 @@
+var searchBtnEl = document.querySelector("#search-btn");
+var zipCodeEl = document.querySelector("#zip-code");
+var radiusEl = document.querySelector("#search-radius");
+var historyEl = document.querySelector("#search-history");
+var searchResultsEl = document.querySelector("#search-results");
+var cardData = [];
+var searchHistory = JSON.parse(localStorage.getItem("search-history"));
+console.log(searchHistory);
+if (searchHistory === null) {
+  searchHistory = [];
+};
+
+var locationApi = function(searchTerm) {
+  var apiUrl = "https://us1.locationiq.com/v1/search.php?key=pk.ba9a631a2a6a1d6a180f349a092cf72b&q=" + searchTerm + "&format=json";
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+        var lat = parseFloat(data[0].lat).toFixed(4);
+        var lon = parseFloat(data[0].lon).toFixed(4);
+        console.log(lat);
+        console.log(lon);
+        pizzaSearchEl(lat, lon);
+      });
+    } else {
+      alert("Error: City not found");
+    }
+  })
+};
+
+searchBtnEl.addEventListener("click", function(event) {
+  event.preventDefault();
+    var searchTerm = zipCodeEl.value;
+    searchHistory.push(searchTerm);
+    locationApi(searchTerm);
+    displaySearchHistory(searchTerm);
+    setLocalStorage(searchTerm);
+    pizzaSearchEl();
+    //displaySearchResults(searchTerm);
+})
+
+var setLocalStorage = function() {
+  localStorage.setItem("search-history", JSON.stringify(searchHistory));
+  if (searchHistory !== "undefined") {
+    return;
+  }
+  displaySearchHistory();
+};
+
+var displaySearchHistory = function(searchTerm) {
+  var historyBtn = document.createElement("button");
+  historyBtn.innerHTML = searchTerm;
+  historyBtn.setAttribute("class", "button is-warning is-rounded history");
+  historyEl.appendChild(historyBtn);
+  historyBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(event.target);
+    //displaySearchResults()
+  });
+};
+
+searchHistory.forEach(function(searchHistory) {
+  displaySearchHistory(searchHistory);
+});
+
+
+var pizzaSearchEl = function (lat, lon) {
+  
+  const options = {
+      method: 'GET',
+      headers: {
+          Accept: 'application/json',
+          Authorization: 'fsq3qsYn7YLzG3qpDo0M38r99P7w7IKfqk5K5aPX9rbTkok='
+      }
+  };
+console.log(lat, lon);
+if (lat && lon) {
+
+
+    fetch('https://api.foursquare.com/v3/places/search?ll=' + lat + ',' + lon + '&radius=40233&categories=13064', options).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+
+            for (i = 0; i < data.results.length; i++) {
+              var card = document.createElement("div");
+              card.setAttribute("class", "card");
+              var cardContent = document.createElement("div");
+              cardContent.setAttribute("class", "content");
+              var name = document.createElement("h2");
+              name.setAttribute("class", "title");
+              name.innerHTML = data.results[i].name;
+              var address = document.createElement("p");
+              address.innerHTML = data.results[i].location.address + " " +  data.results[i].location.locality + " " + data.results[i].location.region + " " + data.results[i].location.postcode;
+              var cardBtn = document.createElement("button");
+              cardBtn.setAttribute("class", "button is-link");
+              cardBtn.innerHTML = "Directions";
+              cardContent.appendChild(cardBtn);
+              cardContent.appendChild(name);
+              cardContent.appendChild(address);
+              card.appendChild(cardContent);
+              searchResultsEl.appendChild(card);
+              var cardResult = {
+                "name" : data.results[i].name,
+                "address" : data.results[i].location.address,
+                "city" : data.results[i].location.locality,
+                "state" : data.results[i].location.region,
+                "zipCode" : data.results[i].location.postcode
+            }
+
+            
+            
+            cardData.push(cardResult); 
+            }    
+   
+        });
+    });
+  }
+};
